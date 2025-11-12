7010ad559e0ff50522290add9a0c79f6c6cb3b63
Daniel Lunev <lunev.danik@gmail.com>
2025-11-12 20:02:14 +0300

    feature/ cors support

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const path = require('path')
-
+const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 3001
 
+app.use(cors())
 app.use(express.json())
 app.use(express.static('public'))
 
