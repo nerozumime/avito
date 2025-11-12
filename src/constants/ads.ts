547f79cd4bd1d30c5891e156ff582f9ee2c9a009
Daniel Lunev <lunev.danik@gmail.com>
2025-11-12 23:03:13 +0300

    feature/ added: ad-status map

---
+++ src/constants/ads.ts
@@ -0,0 +1,14 @@
+export const AD_STATUS = {
+  pending: {
+    color: 'grey',
+    label: 'На модерации',
+  },
+  approved: {
+    color: 'green',
+    label: 'Одобрено',
+  },
+  rejected: {
+    color: 'red',
+    label: 'Отклонено',
+  },
+} as const
