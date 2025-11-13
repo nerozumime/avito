7f99c66644c253734e9f9953898ab3ee2375f4a3
Daniel Lunev <lunev.danik@gmail.com>
2025-11-13 03:16:25 +0300

    feature/ added: detailed ad page

--- src/components/detailed-ad/detailed-ad.tsx
+++ src/components/detailed-ad/detailed-ad.tsx
@@ -1,6 +1,24 @@
-import { useParams } from 'react-router-dom'
+import { useLocation, useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router'
+import { Gallery } from '../../widgets/gallery/gallery.tsx'
 
 export function DetailedAd() {
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const from = location.state?.from || '/list'
+
   const { id } = useParams()
-  return <div>Объявление {id}</div>
+  function handleReturnToList() {
+    navigate(from)
+  }
+  return (
+    <div>
+      <Gallery
+        images={['/public/image-placeholder.jpeg', '/public/image-placeholder.jpeg', '/public/image-placeholder.jpeg']}
+      />
+      <div>Объявление {id}</div>
+      <button onClick={handleReturnToList}>К списку</button>
+    </div>
+  )
 }
