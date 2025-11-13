8e51b1e33c9682dc58f0d0394604899a34484eeb
Daniel Lunev <lunev.danik@gmail.com>
2025-11-13 22:53:04 +0300

    feature/ fetch categories api

--- src/api/ads-api/stats-api.ts
+++ src/api/ads-api/stats-api.ts
@@ -1,5 +1,11 @@
 import { API_BASE_URL } from '../../constants/ads.ts'
-import type { IModeratorActivity, IModeratorDecisions, IModeratorSummary, TStatsPeriod } from '../../types/stats-api.ts'
+import type {
+  IModeratorActivity,
+  IModeratorDecisions,
+  IModeratorSummary,
+  TChartsCategories,
+  TStatsPeriod,
+} from '../../types/stats-api.ts'
 
 export const StatsApi = {
   fetchStats: async (period: TStatsPeriod): Promise<IModeratorSummary> => {
@@ -23,6 +29,15 @@ export const StatsApi = {
   fetchDecisions: async (period: TStatsPeriod): Promise<IModeratorDecisions> => {
     try {
       const response = await fetch(`${API_BASE_URL}/stats/chart/decisions?period=${period}`)
+      return await response.json()
+    } catch (error: any) {
+      throw new Error(error)
+    }
+  },
+
+  fetchCategories: async (period: TStatsPeriod): Promise<TChartsCategories> => {
+    try {
+      const response = await fetch(`${API_BASE_URL}/stats/chart/categories?period=${period}`)
       return await response.json()
     } catch (error: any) {
       throw new Error(error)
