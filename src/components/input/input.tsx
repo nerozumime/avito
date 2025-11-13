6b6e56dadaefcb80bab998950d0acb9101d9cd47
Daniel Lunev <lunev.danik@gmail.com>
2025-11-14 02:53:02 +0300

    feature/ custom input

---
+++ src/components/input/input.tsx
@@ -0,0 +1,31 @@
+import { type ChangeEventHandler } from 'react'
+import styles from './input.module.css'
+import clsx from 'clsx'
+import { CheckboxIcon } from '../icons/checkbox-icon/checkbox-icon.tsx'
+
+interface InputProps {
+  checked: boolean
+  onChange: ChangeEventHandler<HTMLInputElement>
+  name: string
+  value: string
+  label: string
+  type: 'radio' | 'checkbox' | 'dropdown'
+}
+
+export function Input({ checked, onChange, name, value, label }: InputProps) {
+  return (
+    <label className={styles['label-ui']}>
+      <input
+        name={name}
+        type={'checkbox'}
+        checked={checked}
+        onChange={onChange}
+        className={clsx(styles['input'])}
+        tabIndex={-1}
+        value={value}
+      />
+      <CheckboxIcon checked={checked} type={'checkbox'} styleClass={styles.checkbox} />
+      <span className={styles['span-ui']}>{label}</span>
+    </label>
+  )
+}
