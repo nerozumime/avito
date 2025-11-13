f05334b9a2d7ed66f268d286f819ffcf4210e9ca
Daniel Lunev <lunev.danik@gmail.com>
2025-11-13 18:28:32 +0300

    feature/ moderator stats page (WIP)

--- src/pages/moderator-stats/moderator-stats.tsx
+++ src/pages/moderator-stats/moderator-stats.tsx
@@ -1,3 +1,19 @@
+import { useEffect, useState } from 'react'
+import { StatsApi } from '../../api/ads-api/stats-api.ts'
+import type { TStatsPeriod } from '../../types/stats-api.ts'
+
 export function ModeratorStats() {
+  // const [stats, setStats] = useState(null)
+  // const [activityData, setActivityData] = useState([])
+  // const [decisionsData, setDecisionsData] = useState([])
+  // const [categoriesData, setCategoriesData] = useState([])
+  const [period, setPeriod] = useState<TStatsPeriod>('week')
+  // const [loading, setLoading] = useState(false)
+  // const [error, setError] = useState('')
+
+  useEffect(() => {
+    StatsApi.fetchStats(period)
+    StatsApi.fetchActivity(period)
+  }, [])
   return <div>Статистика модератора</div>
 }
