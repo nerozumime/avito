4fde12247d69acf8effe23886ebda6665b23d5a4
Daniel Lunev <lunev.danik@gmail.com>
2025-11-16 00:02:00 +0300

    feature/ moderation-item component added for history of moderation in detailed ad page

---
+++ src/components/moderation-item/moderation-item.tsx
@@ -0,0 +1,31 @@
+import { AD_STATUS, STATUS_COLOR_MAP } from '../../constants/constants.ts'
+import type { IModerationHistory } from '../../types/ads-api.ts'
+import style from './moderation-item.module.css'
+
+interface ModerationItemProps {
+  moderation: IModerationHistory
+}
+export function ModerationItem({ moderation }: ModerationItemProps) {
+  const data = new Date(moderation.timestamp)
+  return (
+    <span className={style.item}>
+      {`Модератор: ${moderation.moderatorName} ${data.toLocaleDateString('ru-RU')}
+                в ${data.getHours()}:${data.getMinutes()}`}
+      <span className={style.status} style={{ backgroundColor: STATUS_COLOR_MAP[moderation.action].backgroundColor }}>
+        {AD_STATUS[moderation.action]?.label}
+      </span>
+      {moderation.action !== 'approved' && (
+        <>
+          <br />
+          {`По причине: ${moderation.reason}`}
+        </>
+      )}
+      {moderation.comment && (
+        <>
+          <br />
+          {`Комментарий: ${moderation.comment}`}
+        </>
+      )}
+    </span>
+  )
+}
