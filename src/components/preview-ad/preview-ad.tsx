3815579b505bccb3e07d9447b562d8582715754b
Daniel Lunev <lunev.danik@gmail.com>
2025-11-15 20:44:02 +0300

    feature/ preview ad added styles and html refactor

--- src/components/preview-ad/preview-ad.tsx
+++ src/components/preview-ad/preview-ad.tsx
@@ -2,9 +2,10 @@ import type { IAd } from '../../types/ads-api.ts'
 import style from './preview-ad.module.css'
 import { useNavigate } from 'react-router'
 import { useLocation } from 'react-router-dom'
-import { AD_STATUS } from '../../constants/constants.ts'
+import { AD_STATUS, CATEGORY_COLOR_MAP } from '../../constants/constants.ts'
 import { AttentionIcon } from '../icons/attention-icon/attention-icon.tsx'
 import { Button } from '../../widgets/button/button.tsx'
+import clsx from 'clsx'
 
 interface PreviewAdProps {
   ad: IAd
@@ -21,21 +22,25 @@ export function PreviewAd({ ad }: PreviewAdProps) {
   return (
     <article className={style.card}>
       <img className={style.image} alt={ad.title} src={'/public/image-placeholder.jpeg'} />
-      <section className={style.info}>
-        <span className={style.title}>{ad.title.split(':')[1]}</span>
-        <span>{ad.price} ₽</span>
-        <span>{ad.category}</span>
-        <span>{ad.createdAt.split('T')[0]}</span>
-      </section>
-      <section className={style.statuses}>
-        <span style={{ color: status?.color }}>{status?.label}</span>
-        {ad.priority === 'urgent' && (
-          <div title={'Срочный приоритет'}>
-            <AttentionIcon />
-          </div>
-        )}
-      </section>
-      <Button onClick={handleNavigate} styleClass={'button-primary'} text={'Открыть'} />
+      <span className={style.category} style={{ backgroundColor: CATEGORY_COLOR_MAP[ad.category] }}>
+        {ad.category}
+      </span>
+      {ad.priority === 'urgent' && (
+        <div title={'Срочный приоритет'} className={style.priority}>
+          <AttentionIcon />
+        </div>
+      )}
+      <div className={style['info-wrapper']}>
+        <section className={style.info}>
+          <span className={style.title}>{ad.title.split(':')[1]}</span>
+          <span>{ad.price} ₽</span>
+          <span>{`Создано: ${ad.createdAt.split('T')[0]}`}</span>
+        </section>
+        <span className={style.status} style={{ color: status?.color }}>
+          {status?.label}
+        </span>
+      </div>
+      <Button onClick={handleNavigate} styleClass={clsx('button-primary', style['open-button'])} text={'Открыть'} />
     </article>
   )
 }
