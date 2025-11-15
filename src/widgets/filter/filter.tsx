9d85063e1251931bb55298cd4e920c8a18232aa2
Daniel Lunev <lunev.danik@gmail.com>
2025-11-15 20:43:30 +0300

    feature/ filter refactor and added styles

--- src/widgets/filter/filter.tsx
+++ src/widgets/filter/filter.tsx
@@ -1,8 +1,9 @@
-import { AD_STATUS } from '../../constants/constants.ts'
+import { AD_STATUS, STATUS_COLOR_MAP } from '../../constants/constants.ts'
 import { Button } from '../button/button.tsx'
 import type { IFilter } from '../../types/ads-api.ts'
 import type { ChangeEvent, Ref } from 'react'
 import { Input } from '../../components/input/input.tsx'
+import style from './filter.module.css'
 
 interface FilterProps {
   filters: IFilter
@@ -22,11 +23,14 @@ export function Filter({
   handleResetFilters,
 }: FilterProps) {
   return (
-    <div>
-      <span>Фильтры</span>
+    <div className={style.filter}>
+      <div className={style.header}>
+        <span className={style.title}>Фильтры:</span>
+        <Button onClick={handleResetFilters} text={'Сбросить фильтры'} styleClass={style.reset} />
+      </div>
       {/*Секция фильтров статуса*/}
       <section>
-        <fieldset>
+        <fieldset className={style.statuses}>
           <legend>Статусы:</legend>
           <Input
             type='checkbox'
@@ -35,6 +39,7 @@ export function Filter({
             onChange={handleStatusChange}
             checked={filters.status.includes('pending')}
             label={AD_STATUS.pending.label}
+            checkedFill={STATUS_COLOR_MAP.requestChanges.backgroundColor}
           />
           <Input
             type='checkbox'
@@ -43,6 +48,7 @@ export function Filter({
             onChange={handleStatusChange}
             checked={filters.status.includes('approved')}
             label={AD_STATUS.approved.label}
+            checkedFill={STATUS_COLOR_MAP.approved.backgroundColor}
           />
           <Input
             type='checkbox'
@@ -51,19 +57,23 @@ export function Filter({
             onChange={handleStatusChange}
             checked={filters.status.includes('rejected')}
             label={AD_STATUS.rejected.label}
+            checkedFill={STATUS_COLOR_MAP.rejected.backgroundColor}
           />
         </fieldset>
       </section>
       {/*Секция фильтров категории*/}
       <div>
-        <label htmlFor='category-select'>Выбор категории:</label>
+        <label htmlFor='category-select' className={style.label}>
+          Выбор категории:
+        </label>
         <select
           id={'category-select'}
           onChange={handleFilterChange}
           name={'categoryId'}
           value={filters.categoryId ?? ''}
+          className={style.input}
         >
-          <option value=''>Не выбрано</option>
+          <option value=''>Любая категория</option>
           <option value={0}>Электроника</option>
           <option value={1}>Недвижимость</option>
           <option value={2}>Транспорт</option>
@@ -76,28 +86,65 @@ export function Filter({
       </div>
       {/* Секция фильтров по цене */}
       <section>
-        <fieldset>
-          <label>
-            Мин. цена
-            <input type={'number'} name={'minPrice'} value={filters.minPrice ?? ''} onChange={handleFilterChange} />
-          </label>
-          <label>
-            Макс. цена
-            <input type={'number'} name={'maxPrice'} value={filters.maxPrice ?? ''} onChange={handleFilterChange} />
-          </label>
+        <fieldset className={style.prices}>
+          <div>
+            <label className={style.label} htmlFor={'min-price'}>
+              Мин. цена
+            </label>
+            <input
+              id={'min-price'}
+              type={'number'}
+              name={'minPrice'}
+              value={filters.minPrice ?? ''}
+              onChange={handleFilterChange}
+              className={style.input}
+              placeholder={'Мин. цена в ₽'}
+            />
+          </div>
+          <div>
+            <label className={style.label} htmlFor={'max-price'}>
+              Макс. цена
+            </label>
+            <input
+              id={'max-price'}
+              type={'number'}
+              name={'maxPrice'}
+              value={filters.maxPrice ?? ''}
+              onChange={handleFilterChange}
+              className={style.input}
+              placeholder={'Макс. цена в ₽'}
+            />
+          </div>
         </fieldset>
       </section>
       {/* Секция фильтра по названию */}
       <section>
-        <label>
+        <label className={style.label} htmlFor={'search'}>
           Название
-          <input name={'search'} type={'text'} value={filters.search} onChange={handleFilterChange} ref={searchRef} />
         </label>
+        <input
+          id={'search'}
+          name={'search'}
+          type={'text'}
+          value={filters.search}
+          onChange={handleFilterChange}
+          ref={searchRef}
+          className={style.input}
+          placeholder={'Название объявления'}
+        />
       </section>
       {/* Секция сортировки */}
       <section>
-        <span>Сортировать</span>
-        <select onChange={handleSort} name={'sortBy'} value={`${filters.sortBy} ${filters.sortOrder}`}>
+        <label htmlFor={'select-sort'} className={style.label}>
+          Сортировать
+        </label>
+        <select
+          id={'select-sort'}
+          onChange={handleSort}
+          name={'sortBy'}
+          value={`${filters.sortBy} ${filters.sortOrder}`}
+          className={style.input}
+        >
           <option value={'createdAt desc'}>По дате создания (сначала новые)</option>
           <option value={'price desc'}>По цене (по убыванию)</option>
           <option value={'priority desc'}>По приоритету (сначала приоритетные)</option>
@@ -106,7 +153,6 @@ export function Filter({
           <option value={'priority asc'}>По приоритету (сначала обычные)</option>
         </select>
       </section>
-      <Button onClick={handleResetFilters} text={'Сбросить фильтры'} />
     </div>
   )
 }
